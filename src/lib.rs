93d8a47eb67bdb49b68f6623db21c01250db8f97
Michail Denchev <mdenchev@gmail.com>
2022-02-24 16:20:26 -0500

    Add some more docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,30 @@
+//! A way to create a dependency graph of items to be executed.
+//!
+//! # Data and Execution model
+//!
+//! This crate allows you to create a Directed Acyclic Graph of items
+//! and track which items are currently "active". Once an item is marked
+//! as completed, any child nodes of that item that have all their parents
+//! completed are marked as active.
+//!
+//! # Intended uses
+//!
+//! This crate was made for sequencing/scripting events and actions for a game
+//! though it could potentially be used for any other case that can be
+//! represented as a dependency graph.
+//!
+//! # Examples
+//!
+//! TBA
+//!
+//! # Features to add:
+//! - More ergonomic methods for creating graphs, especially ones with parallel
+//! execution
+//! - Optional serde support
+//! - Visual debugging of the graphs
+//! - Visual editting of the graphs
+//! - Methods for garbage collecting completed nodes
+
 use std::collections::HashSet;
 
 use slotmap::{new_key_type, SlotMap};
@@ -15,7 +42,9 @@ pub enum NodeStatus {
     Completed,
 }
 
-/// A node in a sequence graph. Each node represents an Item that needs to be processed
+/// A node in a sequence graph.
+///
+/// Each node represents an Item that needs to be processed
 /// and tracks it's current status. Upon completing an item it checks if children nodes
 /// are ready to be processed.
 #[derive(Debug, Clone)]
